79839182cd9b51b170ea508985e432e9e1dbedad
masudermann <89535005+masudermann@users.noreply.github.com>
2021-09-22 23:50:26 -0700

    Update and rename 1_filter_step.rmd to 1_filter_step.r

--- scripts/1_filter_step.r
+++ scripts/1_filter_step.r
@@ -1,10 +1,7 @@
----
-title: "Filtering data after variant calling analyses"
----
+###Filtering data after variant calling analyses###
 
-The Population Genetics and Genomics in R Primer (https://grunwaldlab.github.io/Population_Genetics_in_R/index.html) was referenced. The unfiltered VCF file and metadata table are also available.  
+#The Population Genetics and Genomics in R Primer (https://grunwaldlab.github.io/Population_Genetics_in_R/index.html) was referenced. The unfiltered VCF file and metadata table are also available.  
 
-```{r, eval=TRUE, include=FALSE}
 library(reshape2)
 library(ggplot2) 
 library(cowplot)
@@ -13,10 +10,8 @@ library(poppr)
 library(ape)
 library(RColorBrewer)
 library(gdata)
-```
 
-Visualization of read depth
-```{r, eval=TRUE, include=TRUE}
+#Visualization of read depth
 pass.vcf = read.vcfR("passalora_4_2.vcf")
 dp <- extract.gt(pass.vcf, element = "DP", as.numeric=TRUE)
 dpf <- melt(dp, varnames=c('Index', 'Sample'), value.name = 'Depth', na.rm=TRUE)
@@ -49,9 +44,9 @@ for(i in 1:myRows){
   myPlots[[i]] <- myPlots[[i]] + theme( panel.grid.minor.y=element_line(color = "#C0C0C0", size=0.2) )
 }
 plot_grid(plotlist = myPlots, nrow = myRows)
-```
-Initial filtering based on read depth
-```{r, eval=TRUE, include=TRUE}
+
+
+#Initial filtering based on read depth
 quants <- apply(dp, MARGIN=2, quantile, probs=c(0.05, 0.95), na.rm=TRUE)
 dp2 <- sweep(dp, MARGIN=2, FUN = "-", quants[1,])
 dp[dp2 < 0] <- NA
@@ -61,29 +56,23 @@ dp[dp < 5] <- NA
 dp[dp > 100] <- NA
 pass.vcf@gt[,-1][ is.na(dp) == TRUE ] <- NA
 pass.vcf
-```
 
-Omitting samples based on percentage of missing data
-```{r, eval=TRUE, include=TRUE}
+#Omitting samples based on percentage of missing data
 myMiss <- apply(dp, MARGIN = 2, function(x){ sum( is.na(x) ) } )
 myMiss <- myMiss / nrow(dp)
 pass.vcf@gt <- pass.vcf@gt[, c(TRUE, myMiss < 0.55)]
 dp <- extract.gt(pass.vcf, element = "DP", as.numeric=TRUE)
 heatmap.bp(dp[1:1000,], rlabels = FALSE)
 pass.vcf
-```
 
-Omitting variants
-```{r, eval=TRUE, include=TRUE}
+#Omitting variants
 dp <- extract.gt(pass.vcf, element = "DP", as.numeric=TRUE)
 myMiss <- apply(dp, MARGIN = 1, function(x){ sum( is.na(x) ) } )
 myMiss <- myMiss / ncol(dp)
 hist(myMiss, col = "#8DD3C7", xlab = "Missingness (%)")
 pass.vcf <- pass.vcf[myMiss < 0.2, ]
-```
 
-Visualizing filtered data
-```{r, eval=TRUE, include=TRUE}
+#Visualizing filtered data
 dp <- extract.gt(pass.vcf, element = "DP", as.numeric=TRUE)
 heatmap.bp(dp[1:1000,], rlabels = FALSE)
 dpf <- melt(dp, varnames = c("Index", "Sample"),
@@ -102,28 +91,23 @@ p <- p + theme(panel.grid.major.y = element_line(color = "#A9A9A9", size = 0.6))
 p <- p + theme(panel.grid.minor.y = element_line(color = "#C0C0C0", size = 0.2))
 p <- p + ylab("Depth (DP)")
 p
-```
 
-Retain only non-heterozygous positions, as well as biallelic and polymorphic positions
-```{r, eval=TRUE, include=TRUE}
+#Retain only non-heterozygous positions, as well as biallelic and polymorphic positions
 #Remove heterozygous positions
 gt <- extract.gt(pass.vcf)
 hets <- is_het(gt)
 is.na(pass.vcf@gt[,-1][is_het(gt)]) <- TRUE
 gt <- extract.gt(pass.vcf)
 
-# Retaining only the biallelic positions 
+#Retaining only the biallelic positions 
 pass.vcf <- pass.vcf[is.biallelic(pass.vcf)]
 # Retaining only the polymorphic sites
 pass.vcf <- pass.vcf[is.polymorphic(pass.vcf, na.omit = T),]
 pass.vcf
-```
 
-Filtering by minor allele frequency
-```{r, eval=TRUE, include=TRUE}
+#Filtering by minor allele frequency
 mymaf <- maf(pass.vcf, element = 2)
 mymaf <- mymaf[mymaf[,4] > 0.05,]
-
 fix <- pass.vcf@fix[pass.vcf@fix[,3] %in% rownames(mymaf), ]
 nrow(fix)
 pass.vcf@fix <- fix
@@ -133,18 +117,13 @@ pass.vcf@fix <- pass.vcf@fix[true_ind, ]
 pass.vcf@gt <- pass.vcf@gt[true_ind, ]
 pass.vcf
 write.vcf(pass.vcf, file = "filteredpfulva.vcf.gz")
-```
 
-Obtain information about % missing data for each sample. This is useful for determining which of the technical replicates to retain or discard in the final analysis
-```{r, eval=TRUE, include=TRUE}
+#Obtain information about % missing data for each sample. This is useful for determining which of the technical replicates to retain or discard in the final analysis
 myMiss <- apply(dp, MARGIN = 2, function(x){ sum(is.na(x)) })
 myMiss <- myMiss/nrow(pass.vcf)
 write.csv(myMiss, file = "missingnesspfulva")
-```
 
-Remove replicate isolates from dataset, based on % missing data
-```{r, eval=TRUE, include=TRUE}
+#Remove replicate isolates from dataset, based on % missing data
 copy1 = read.vcfR("filteredpfulva.vcf.gz")
 copy1@gt <- copy1@gt[, !colnames(copy1@gt) %in% c("pf78-2", "pf81-2", "pf77", "pf79", "19001-2", "17035", "19016-2", "19018-2", "pf92", "17052-2","19006-2")]
 write.vcf(copy1,file = "filteredpfulvanodup.vcf.gz")
-```
